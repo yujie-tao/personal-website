a97e785d5232e910d7d39d39843488f4cc3bb683
yujie-tao <yujietao@outlook.com>
2019-01-11 15:26:07 -0500

    set up header

---
+++ js/events.js
@@ -0,0 +1,31 @@
+// setTimeout(function(){
+//       $('.second').removeClass("fade-in");
+//       $('.setence').removeClass("fade-in");
+//    }, 3200);
+
+// window.counter = function() {
+//     // this refers to the html element with the data-scroll-showCallback tag
+//     var span = this.querySelector('span');
+//     var current = parseInt(span.textContent);
+
+//     span.textContent = current + 1;
+// };
+
+// $(document).ready(
+//     function(){
+//     $('#camp').on('click', function(){
+//     	    $('.campaign').hide();
+//             $('.campaign').fadeIn(1000);
+//         });
+
+
+//     $('#prot').on('click', function(){
+//     	$('.prototype').hide();
+//             $('.prototype').fadeIn(1000);
+//         });
+
+//     });
+
+// AOS.init({
+//         easing: 'ease-in-out-sine'
+//       });
