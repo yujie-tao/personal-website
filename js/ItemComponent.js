68e28b30bc0a15af616e5d7fe6a2875a1f320a07
yujie-tao <yujietao@outlook.com>
2019-06-30 00:03:13 -0700

    add new project

---
+++ js/ItemComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class ItemComponent extends Component {
+	static propTypes = {
+		items: PropTypes.array.isRequired
+	};
+
+	render() {
+		return (
+			<View style={styles.itemsList}>
+				{this.props.items.map((item, index) => {
+					return (
+						<View key={index}>
+							<Text style={styles.itemtext}>{item.name}</Text>
+						</View>
+					);
+				})}
+			</View>
+		);
+	}
